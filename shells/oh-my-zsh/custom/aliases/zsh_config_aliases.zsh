69ed1c91f386809e336d58760439cf2a1b0ff69c
Leon <silenceace@gmail.com>
2025-03-27 00:26:08 +0800

    feat: 添加安装 oh-my-zsh 别名文件的提示信息和使用说明

--- shells/oh-my-zsh/custom/aliases/zsh_config_aliases.zsh
+++ shells/oh-my-zsh/custom/aliases/zsh_config_aliases.zsh
@@ -12,11 +12,15 @@ alias ohmyzsh='mate ~/.oh-my-zsh' # Edit ~/.oh-my-zsh directory in Mate text edi
 alias asg='(){ echo "Search alias.\nUsage:\n asg <alias_name>"; alias_name=${@}; alias | grep $alias_name;}' # Search for aliases
 
 alias install_omz_aliases='() {
+  echo "Installing oh-my-zsh alias files from remote repository."
   local remote_url="https://raw.githubusercontent.com/funnyzak/dotfiles/refs/heads/main/"
   curl -fsSL "${remote_url}shells/oh-my-zsh/tools/install_omz_aliases.sh" | bash -s -- "$@"
 }' # Install oh-my-zsh alias files from a remote repository (Linux/macOS)
 
 alias install_omz_aliases_cn='() {
+  echo "Installing oh-my-zsh alias files from China mirror."
+  echo "You can speed up the download using China mirrors."
+  echo "Usage: export CN=true; install_omz_aliases_cn"
   local remote_url="https://raw.gitcode.com/funnyzak/dotfiles/raw/main/"
   curl -fsSL "${remote_url}shells/oh-my-zsh/tools/install_omz_aliases.sh" | bash -s -- "$@"
 }' # Install oh-my-zsh alias files from a remote repository (China)
