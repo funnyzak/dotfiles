5c1351cc4e7bea7ac3cc0d19bde605a73e97cd4e
Leon <silenceace@gmail.com>
2025-03-27 00:19:12 +0800

    feat: 添加中国用户的 oh-my-zsh 别名安装命令

--- shells/oh-my-zsh/custom/aliases/zsh_config_aliases.zsh
+++ shells/oh-my-zsh/custom/aliases/zsh_config_aliases.zsh
@@ -13,8 +13,10 @@ alias asg='(){ echo "Search alias.\nUsage:\n asg <alias_name>"; alias_name=${@};
 
 alias install_omz_aliases='() {
   local remote_url="https://raw.githubusercontent.com/funnyzak/dotfiles/refs/heads/main/"
-  if [[ "$CN" == "true" ]]; then
-    remote_url = "https://raw.gitcode.com/funnyzak/dotfiles/raw/main/"
-  fi
   curl -fsSL "${remote_url}shells/oh-my-zsh/tools/install_omz_aliases.sh" | bash -s -- "$@"
 }' # Install oh-my-zsh alias files from a remote repository (Linux/macOS)
+
+alias install_omz_aliases_cn='() {
+  local remote_url="https://raw.gitcode.com/funnyzak/dotfiles/raw/main/"
+  curl -fsSL "${remote_url}shells/oh-my-zsh/tools/install_omz_aliases.sh" | bash -s -- --url https://raw.gitcode.com/funnyzak/dotfiles/raw/main/shells/oh-my-zsh/custom/aliases/ "$@"
+}' # Install oh-my-zsh alias files from a remote repository (China)
