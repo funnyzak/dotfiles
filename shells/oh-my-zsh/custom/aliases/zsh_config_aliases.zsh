01b8b4ab0b06a0edf08844fb3e879da5e6a7151e
Leon <silenceace@gmail.com>
2025-03-27 10:02:10 +0800

    chore:update

--- shells/oh-my-zsh/custom/aliases/zsh_config_aliases.zsh
+++ shells/oh-my-zsh/custom/aliases/zsh_config_aliases.zsh
@@ -10,14 +10,3 @@ alias zshrc='source ~/.zshrc' # Source ~/.zshrc to reload configuration
 alias zshconfigp='less ~/.zshrc' # View ~/.zshrc in less
 alias ohmyzsh='mate ~/.oh-my-zsh' # Edit ~/.oh-my-zsh directory in Mate text editor
 alias asg='(){ echo "Search alias.\nUsage:\n asg <alias_name>"; alias_name=${@}; alias | grep $alias_name;}' # Search for aliases
-
-alias install_omz_aliases='() {
-  REMOTE_URL_PREFIX="https://raw.githubusercontent.com/funnyzak/dotfiles/refs/heads/main/"
-  REMOTE_URL_PREFIX_CN="https://raw.gitcode.com/funnyzak/dotfiles/raw/main/"
-  if curl -s --connect-timeout 2 "$REMOTE_URL_PREFIX_CN" >/dev/null 2>&1; then
-    REMOTE_URL_PREFIX=$REMOTE_URL_PREFIX_CN
-  fi
-  echo "Installing oh-my-zsh alias files from remote repository."
-  echo "Using remote URL: $REMOTE_URL_PREFIX"
-  curl -fsSL "${REMOTE_URL_PREFIX}shells/oh-my-zsh/tools/install_omz_aliases.sh" | bash -s -- "$@"
-}' # Install oh-my-zsh alias files from a remote repository (Linux/macOS)
