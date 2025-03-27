ee9ce7f52db9a1e1d91e4f99eb3221ca69db5a02
Leon <silenceace@gmail.com>
2025-03-27 08:27:03 +0800

    chore:update

--- shells/oh-my-zsh/custom/aliases/help_aliases.zsh
+++ shells/oh-my-zsh/custom/aliases/help_aliases.zsh
@@ -1,10 +1,10 @@
-alias cheatsheet='() {
+alias cs='() {
   CHEATSHEET_REMOTE_URL="https://raw.githubusercontent.com/funnyzak/dotfiles/main/utilities/shell/cheatsheet.sh"
   echo "Command cheatsheet tool.\nUsage:\n cheatsheet [command] to view specific command\n cheatsheet -l to list all supported commands"
   curl -sSL "$CHEATSHEET_REMOTE_URL" | bash -s -- "$@"
 }'
 
-alias cn_cheatsheet='() {
+alias cn_cs='() {
   CHEATSHEET_REMOTE_URL="https://raw.gitcode.com/funnyzak/dotfiles/raw/main/utilities/shell/cheatsheet.sh"
   echo "Command cheatsheet tool.\nUsage:\n cheatsheet [command] to view specific command\n cheatsheet -l to list all supported commands"
   curl -sSL "$CHEATSHEET_REMOTE_URL" | bash -s -- "$@"
