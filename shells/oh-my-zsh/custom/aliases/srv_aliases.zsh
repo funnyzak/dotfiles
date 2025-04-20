557be5289d5e18ed1b6687005362b0a3a721c0b9
Leon <silenceace@gmail.com>
2025-04-20 20:03:58 +0800

    feat: 添加srv-create-deploy命令示例，优化用户创建流程

--- shells/oh-my-zsh/custom/aliases/srv_aliases.zsh
+++ shells/oh-my-zsh/custom/aliases/srv_aliases.zsh
@@ -887,6 +887,8 @@ alias srv-create-deploy='() {
   echo -e "Examples:"
   echo -e "  srv-create-deploy deployuser"
   echo -e "  srv-create-deploy myapp myappgroup /var/www/myapplication --shell"
+  echo -e "  srv-create-deploy newuser mygroup /var/www/newapp --no-password"
+  echo -e "  srv-create-deploy anotheruser anothergroup /var/www/anotherapp --shell"
 
   # Check if username is provided
   if [ -z "$1" ]; then
